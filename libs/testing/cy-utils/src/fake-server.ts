df98536bc2aa4110bc85fe895806ba82324f562f
yymoroz3 <yymoroz3@yandex.ru>
2022-09-18 13:42:53 +0300

    test: fix mock server warnings

--- libs/testing/cy-utils/src/fake-server.ts
+++ libs/testing/cy-utils/src/fake-server.ts
@@ -3,7 +3,9 @@ import {RequestHandler, setupWorker} from 'msw';
 export const worker = setupWorker();
 
 export const startMockServer = () => {
-  cy.wrap(worker.start(), {log: false}).log("Mock server started");
+  cy.wrap(worker.start({
+    onUnhandledRequest: 'bypass' // ignore unknown requests
+  }), {log: false}).log("Mock server started");
 };
 
 export const stopMockServer = () => {
